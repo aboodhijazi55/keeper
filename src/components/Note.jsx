9e9399b5d17e577d7958f6be0f0a0c606bd9887f
abood <abodh778@gmail.com>
2024-11-25 09:03:53 +0300

    first commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Note() {
+    return <div className="note">
+        <h1>first note</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
+    </div>
+}
+
+export default Note
